146fbace0fa4e1ad9e1bae177caeb28b536a9aa8
hczhang <hczhang@vw-mobvoi.com>
2021-11-14 13:11:33 +0800

    modify code

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include "add.h"
 #include "stdio.h"
+#include "sub.h"
 int main(){
 printf("hello world\n");
-printf("add value:%d",add(5,3));
+printf("add value:%d\n",add(5,3));
+printf("sub value:%d\n",sub(5,3));
 
 }
